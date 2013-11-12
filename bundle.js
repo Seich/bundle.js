5e19f6f07f17bb64d0b6c65d6739ecbad230632f
Sergio Díaz <seich@martianwabbit.com>
2013-11-11 21:40:04 -0600

    re-indenting code.

--- bundle.js
+++ bundle.js
@@ -1,118 +1,118 @@
 ;(function (root, factory) {
-	'use strict';
-	
+    'use strict';
+    
     if (typeof define === 'function' && define.amd) {
         define(['jquery', 'handlebars'], factory);
     } else {
         root.Bundle = factory(jQuery, Handlebars);
     }
 }(this, function ($, handlebars) {
-	'use strict';
+    'use strict';
 
-	var bundles = {};
-	var templates = {};
+    var bundles = {};
+    var templates = {};
 
-	var default_config = {
-		render_method: 'html'
-	};
+    var default_config = {
+        render_method: 'html'
+    };
 
-	var Bundle = function(bundle_name, bundle) {
-		bundles[bundle_name] = bundle;
-	};
+    var Bundle = function(bundle_name, bundle) {
+        bundles[bundle_name] = bundle;
+    };
 
-	Bundle.open = function(bundle_name, element, data, config) {
-		var bundle = bundles[bundle_name];
-		var $element = $(element);
-		config = config || {};
-		bundle.defaults = bundle.defaults || {};
+    Bundle.open = function(bundle_name, element, data, config) {
+        var bundle = bundles[bundle_name];
+        var $element = $(element);
+        config = config || {};
+        bundle.defaults = bundle.defaults || {};
 
-		var defaults = $.extend({}, bundle.defaults, data);
-		config = $.extend({}, default_config, bundle.config, config);
+        var defaults = $.extend({}, bundle.defaults, data);
+        config = $.extend({}, default_config, bundle.config, config);
 
-		var templateDefer = new $.Deferred();
-		var dataDefer = new $.Deferred();
+        var templateDefer = new $.Deferred();
+        var dataDefer = new $.Deferred();
 
-		if ('template' in bundle) {
-			var getting_template = $.get(bundle.template);
+        if ('template' in bundle) {
+            var getting_template = $.get(bundle.template);
 
-			getting_template.done(function(data) {
-				var compile = handlebars.compile || handlebars.default.compile;
-				var template = compile(data);
-				templates[bundle.template] = template;
+            getting_template.done(function(data) {
+                var compile = handlebars.compile || handlebars.default.compile;
+                var template = compile(data);
+                templates[bundle.template] = template;
 
-				templateDefer.resolve();
-			});
-		} else {
-			templateDefer.resolve();
-		}
+                templateDefer.resolve();
+            });
+        } else {
+            templateDefer.resolve();
+        }
 
-		if ('data_src' in bundle) {
-			var getting_data = $.getJSON(_parseBundleDataUrl(bundle, defaults));
+        if ('data_src' in bundle) {
+            var getting_data = $.getJSON(_parseBundleDataUrl(bundle, defaults));
 
-			getting_data.done(function(data) {
-				defaults = $.extend({}, defaults, data);
-				dataDefer.resolve();
-			});
-		} else {
-			dataDefer.resolve();
-		}
+            getting_data.done(function(data) {
+                defaults = $.extend({}, defaults, data);
+                dataDefer.resolve();
+            });
+        } else {
+            dataDefer.resolve();
+        }
 
-		$.when(templateDefer, dataDefer).done(function() {
-			_render(bundle, $element, defaults, config);
-		});
-	};
+        $.when(templateDefer, dataDefer).done(function() {
+            _render(bundle, $element, defaults, config);
+        });
+    };
 
-	var _render = function(bundle, element, defaults, config) {
-		var template = templates[bundle.template];
+    var _render = function(bundle, element, defaults, config) {
+        var template = templates[bundle.template];
 
-		var render = function() {
-			if (typeof template !== 'function') {
-				throw new Error('Trying to call the render method without a template.');
-			}
+        var render = function() {
+            if (typeof template !== 'function') {
+                throw new Error('Trying to call the render method without a template.');
+            }
 
-			var $template = $(template(defaults));
+            var $template = $(template(defaults));
 
-			if ('events' in bundle) {
-				$.each(bundle.events, function(event, callback) {
-					var query = event.split(' ');
-					event = query.pop();
-					query = query.join(' ');
+            if ('events' in bundle) {
+                $.each(bundle.events, function(event, callback) {
+                    var query = event.split(' ');
+                    event = query.pop();
+                    query = query.join(' ');
 
-					$template.find(query).on(event, function(event) {
-						var $element = $(this);
-						
-						callback.call(bundle, event, $element);
+                    $template.find(query).on(event, function(event) {
+                        var $element = $(this);
+                        
+                        callback.call(bundle, event, $element);
 
-					});
-				});
-			}
+                    });
+                });
+            }
 
-			element[config.render_method]($template);
-		};
+            element[config.render_method]($template);
+        };
 
-		if ('render' in bundle) {
-			bundle._render = render;
-			bundle.render.call(bundle, element, defaults, template);
-		} else {
-			render();
-		}
+        if ('render' in bundle) {
+            bundle._render = render;
+            bundle.render.call(bundle, element, defaults, template);
+        } else {
+            render();
+        }
 
-		if ('init' in bundle) {
-			bundle.init.call(bundle, element, defaults);
-		}
-	};
+        if ('init' in bundle) {
+            bundle.init.call(bundle, element, defaults);
+        }
+    };
 
-	var _parseBundleDataUrl = function(bundle, defaults) {
-		var url = bundle.data_src;
-		var matcher = /{{(\w*)}}/gi;
+    var _parseBundleDataUrl = function(bundle, defaults) {
+        var url = bundle.data_src;
+        var matcher = /{{(\w*)}}/gi;
 
-		var match;
-		while((match = matcher.exec(bundle.data_src))) {
-			url = url.replace(match[0], defaults[match[1]]);
-		}
+        var match;
+        while((match = matcher.exec(bundle.data_src))) {
+            url = url.replace(match[0], defaults[match[1]]);
+        }
 
-		return url;
-	};
+        return url;
+    };
 
-	return Bundle;
+    return Bundle;
 }));
